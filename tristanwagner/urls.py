e406087f483b879bab17c1fedf4f4ada6999a424
twagner000 <twagner000@users.noreply.github.com>
2015-07-18 11:29:56 -0700

    brought over home page from old site

--- tristanwagner/urls.py
+++ tristanwagner/urls.py
@@ -1,10 +1,12 @@
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
+admin.autodiscover()
 
-urlpatterns = [
-    # Examples:
-    # url(r'^$', 'tristanwagner.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = patterns('',
+    url(r'^$', 'tristanwagner.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
-]
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page':'home'}, name='logout'),
+    #url(r'^games/', include('bgg_data.urls', namespace='bgg_data')),
+    #url(r'^checklist/', include('checklist.urls', namespace='checklist')),
+)
