e8a03a00e3f2a444019d636f71aa96ff597b697f
twagner000 <twagner000@users.noreply.github.com>
2015-07-26 17:38:13 -0700

    added max tax rates to widget; set increments to 5

--- deveconsim/forms.py
+++ deveconsim/forms.py
@@ -17,9 +17,9 @@ class BudgetForm(forms.ModelForm):
                   'svc_health':'Healthcare Funding',
                   'svc_education':'Education Funding',
                   'svc_security':'Security Funding',}
-        widgets = {'tax_cocoa':forms.TextInput(attrs={'class':'form-control touchspin-pct'}),
-                   'tax_lower':forms.TextInput(attrs={'class':'form-control touchspin-pct'}),
-                   'tax_upper':forms.TextInput(attrs={'class':'form-control touchspin-pct'}),
+        widgets = {'tax_cocoa':forms.TextInput(attrs={'class':'form-control touchspin-pct-30'}),
+                   'tax_lower':forms.TextInput(attrs={'class':'form-control touchspin-pct-70'}),
+                   'tax_upper':forms.TextInput(attrs={'class':'form-control touchspin-pct-70'}),
                    'svc_health':forms.TextInput(attrs={'class':'form-control touchspin-pct'}),
                    'svc_education':forms.TextInput(attrs={'class':'form-control touchspin-pct'}),
                    'svc_security':forms.TextInput(attrs={'class':'form-control touchspin-pct'}),}
