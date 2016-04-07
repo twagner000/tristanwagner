05dd706c22402bdec857825ea3ada25fa5dfca17
twagner000 <twagner000@users.noreply.github.com>
2016-04-06 23:35:21 -0700

    add Isle of Skye puzzle
    Add a hard-coded check for the puzzle answer.

--- puzzles/urls.py
+++ puzzles/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^$', 'puzzles.views.index', name='index'),
+    url(r'^solved/$', 'puzzles.views.solved', name='solved'),
+    url(r'^wrong/$', 'puzzles.views.wrong', name='wrong'),
 )
--- puzzles/views.py
+++ puzzles/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, 'puzzles/index.html', {})
+    
+def solved(request):
+    if request.POST.get('solution', '').lower() == 'scotch':
+        return render(request, 'puzzles/solved.html', {})
+    else:
+        return redirect('puzzles:wrong')
+        
+def wrong(request):
+    return render(request, 'puzzles/wrong.html')
