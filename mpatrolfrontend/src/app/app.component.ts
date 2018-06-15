bea545fc7439b438bccea21df976ef5ae3f1d835
twagner000 <twagner000@users.noreply.github.com>
2018-06-14 22:47:13 -0700

    started angular app

--- mpatrolfrontend/src/app/app.component.ts
+++ mpatrolfrontend/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+    import { Http, Response } from '@angular/http';
+    import { Observable } from 'rxjs';
+    import 'rxjs/operators';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'app';
-}
+    @Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+    })
+    export class AppComponent {
+    title = 'app';
+    url : string = 'http://localhost:8000/mpatrol/api/creature/';
+
+
+    constructor(private http : Http){}
+    public getCreatures(){
+
+        this.http.get(this.url).toPromise().then((res)=>{
+            console.log(res.json());
+        });
+
+    }
+    }
--- mpatrolfrontend/src/app/app.module.ts
+++ mpatrolfrontend/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+    import { NgModule } from '@angular/core';
+    import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+    import { AppComponent } from './app.component';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+    @NgModule({
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule ,
+        HttpModule //this is the HTTP module
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+    })
+    export class AppModule { }
