852895ea7149f2e35d770f1a74f500592de0a39c
twagner000 <twagner000@users.noreply.github.com>
2018-07-15 22:43:10 -0700

    crispy password change form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 from crispy_forms.bootstrap import FormActions
@@ -10,3 +10,10 @@ class CrispyAuthenticationForm(AuthenticationForm):
         self.helper = FormHelper(self)
         self.helper.form_method = 'post'
         self.helper.layout.append(FormActions(Submit('submit', 'Log in')))
+
+class CrispyPasswordChangeForm(PasswordChangeForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_method = 'post'
+        self.helper.layout.append(FormActions(Submit('submit', 'Change my password')))
--- config/urls.py
+++ config/urls.py
@@ -4,21 +4,20 @@ from django.views.generic.base import TemplateView, RedirectView
 from django.contrib.auth import views as auth_views
 from rest_framework.authtoken import views as rest_framework_views
 
-from accounts.forms import CrispyAuthenticationForm
+from accounts.forms import CrispyAuthenticationForm, CrispyPasswordChangeForm
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='pages/home.html'), name='home'),
     path('admin/', admin.site.urls),
-    path('accounts/login/', auth_views.LoginView.as_view(authentication_form=CrispyAuthenticationForm), name='login'),
-    path('accounts/logout/', auth_views.logout, name='logout'),
     path('accounts/token/', rest_framework_views.obtain_auth_token, name='auth-token'),
+    path('accounts/login/', auth_views.LoginView.as_view(authentication_form=CrispyAuthenticationForm), name='login'),
+    path('accounts/password_change/', auth_views.PasswordChangeView.as_view(form_class=CrispyPasswordChangeForm), name='password_change'),
+    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    #path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    #path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    #path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     #path('accounts/', include('registration.backends.hmac.urls')), #https://github.com/ubernostrum/django-registration/blob/master/docs/hmac.rst
-    #accounts/password_change/ [name='password_change']
-    #accounts/password_change/done/ [name='password_change_done']
-    #accounts/password_reset/ [name='password_reset']
-    #accounts/password_reset/done/ [name='password_reset_done']
-    #accounts/reset/<uidb64>/<token>/ [name='password_reset_confirm']
-    #accounts/reset/done/ [name='password_reset_complete']
+    path('accounts/', include('django.contrib.auth.urls')),
     path('checklist/', include(('checklist.urls', 'checklist'), namespace='checklist')),
     path('games/', include(('games.urls', 'games'), namespace='games')),
     path('puzzles/', include(('puzzles.urls', 'puzzles'), namespace='puzzles')),
