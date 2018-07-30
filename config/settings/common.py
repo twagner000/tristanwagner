3fbe2b68a33e6ab9b3b3d9f9fee63f98cd201c0e
twagner000 <twagner000@users.noreply.github.com>
2018-07-29 19:57:48 -0700

    mpatrolfrontend static

--- config/settings/common.py
+++ config/settings/common.py
@@ -147,7 +147,7 @@ STATIC_URL = '/static/'
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = (
     str(APPS_DIR.path('static')),
-    str(ROOT_DIR.path('mpatrolfrontend/dist')),
+    #str(ROOT_DIR.path('mpatrolfrontend/dist')),
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
