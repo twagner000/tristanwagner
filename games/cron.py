109211c2332c46b87a898354a1fc8f605000b10f
twagner000 <twagner000@users.noreply.github.com>
2019-03-11 22:20:35 -0700

    Update cron.py

--- games/cron.py
+++ games/cron.py
@@ -5,7 +5,6 @@ from . import models
 from django.conf import settings
 import requests
 import xml.etree.ElementTree as ET
-import pytz
 
 class RecentBGGPlaysCronJob(CronJobBase):
     schedule = Schedule(run_every_mins=60)
@@ -14,8 +13,7 @@ class RecentBGGPlaysCronJob(CronJobBase):
     def do(self):
         xml = requests.get("https://boardgamegeek.com/xmlapi2/plays", params={
             'username':settings.BGG_USER,
-            #'mindate':(datetime.date.today() - datetime.timedelta(days=30)).strftime('%Y-%m-%d')
-            'mindate':'2019-01-01'
+            'mindate':(datetime.date.today() - datetime.timedelta(days=30)).strftime('%Y-%m-%d')
             })
         root = ET.fromstring(xml.text.encode('utf8'))
         for p in root.iter('play'):
@@ -23,8 +21,7 @@ class RecentBGGPlaysCronJob(CronJobBase):
             item = p.find('item')
             play.bgg_game_id = item.attrib['objectid']
             play.game_name = item.attrib['name']
-            d = datetime.datetime.strptime(p.attrib['date'], '%Y-%m-%d').date()
-            play.date = datetime.datetime(d.year, d.month, d.day, tzinfo=pytz.UTC)
+            play.date = datetime.datetime.strptime(p.attrib['date'], '%Y-%m-%d').date()
             play.quantity = int(p.attrib['quantity'])
             play.save()
         
