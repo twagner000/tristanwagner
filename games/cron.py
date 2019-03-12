bdec5eff62895bde6588fcccf468022e5dc8bbe7
twagner000 <twagner000@users.noreply.github.com>
2019-03-11 22:09:16 -0700

    Update cron.py

--- games/cron.py
+++ games/cron.py
@@ -14,8 +14,7 @@ class RecentBGGPlaysCronJob(CronJobBase):
         xml = requests.get("https://boardgamegeek.com/xmlapi2/plays", params={
             'username':settings.BGG_USER,
             #'mindate':(datetime.date.today() - datetime.timedelta(days=30)).strftime('%Y-%m-%d')
-            'mindate':'2018-07-01',
-            'maxdate':'2019-01-01'
+            'mindate':'2019-01-01'
             })
         root = ET.fromstring(xml.text.encode('utf8'))
         for p in root.iter('play'):
@@ -23,7 +22,7 @@ class RecentBGGPlaysCronJob(CronJobBase):
             item = p.find('item')
             play.bgg_game_id = item.attrib['objectid']
             play.game_name = item.attrib['name']
-            play.date = datetime.datetime.strptime(p.attrib['date'], '%Y-%m-%d').date()
+            play.date = datetime.datetime.strptime(p.attrib['date'], '%Y-%m-%d')
             play.quantity = int(p.attrib['quantity'])
             play.save()
         
