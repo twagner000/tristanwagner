3983b8e6b41724be28ae99635044b70a8019c0d9
twagner000 <twagner000@users.noreply.github.com>
2019-06-26 13:30:33 -0700

    remove unused imports

--- timetracker/src/components/Entry.js
+++ timetracker/src/components/Entry.js
@@ -1,5 +1,5 @@
 import React from "react";
-import PropTypes from "prop-types";
+//import PropTypes from "prop-types";
 import key from "weak-key";
 import { format } from 'date-fns';
 import { Link, withRouter } from "react-router-dom";
