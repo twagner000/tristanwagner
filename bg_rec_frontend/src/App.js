c6bb2eebe02d86fae0542c40ab118f6c5aa90744
twagner000 <twagner000@users.noreply.github.com>
2019-08-08 16:38:05 -0700

    format results

--- bg_rec_frontend/src/App.js
+++ bg_rec_frontend/src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import './App.css';
 
+
 import 'react-bulma-components/dist/react-bulma-components.min.css';
-import { Section, Container, Heading, Form, Icon, Columns, Box, Content} from 'react-bulma-components';
+import { Section, Container, Heading, Form, Icon, Columns, Box, Content, Level, Card, Image} from 'react-bulma-components';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import axios from 'axios';
 
 const BASE_URL = '/bg_rec';
 
-class Info extends React.Component {
-	render() {
-		return (<p className="content">Choose a game to get recommendations of similar games.</p>);
-	}
+function Info(props) {
+	return (<p className="content">Choose a game to get recommendations of similar games.</p>);
 }
 
 class Search extends React.Component {
@@ -66,35 +65,50 @@ class Search extends React.Component {
 }
 
 class Game extends React.Component {
+	static players(game) {
+		return (game.minplayers && game.maxplayers && game.minplayers !== game.maxplayers)
+			? `${game.minplayers}-${game.maxplayers}`
+			: `${game.minplayers}`;
+	}
+	
+	static playtime(game) {
+		return (game.minplaytime && game.maxplaytime && game.minplaytime !== game.maxplaytime)
+			? `${game.minplaytime}-${game.maxplaytime}`
+			: `${game.playingtime}`;
+	}
+	
 	render() {
 		let neigh = this.props.game_neighbor;
 		return (
-			<Box>
-				<h4><Link to={`/game/${neigh.neighbor.objectid}/`}>{neigh.neighbor.name}</Link></h4>
-				<Columns gapless>
-					<Columns.Column size="one-third">
-						<Icon><i className="fas fa-arrows-alt-h"></i></Icon>{neigh.distance.toFixed(3)}
-					</Columns.Column>
-					<Columns.Column size="one-third">
-						<Icon><i className="fas fa-globe"></i></Icon>{neigh.neighbor.usersrated}
-					</Columns.Column>
-					<Columns.Column size="one-third">
-						<a href={`https://boardgamegeek.com/boardgame/${neigh.neighbor.objectid}`}><Icon><i className="fas fa-external-link-alt"></i></Icon> BGG</a>
-					</Columns.Column>
-					
-					<Columns.Column size="one-third">
-						<Icon><i className="fas fa-users"></i></Icon>{neigh.neighbor.minplayers}-{neigh.neighbor.maxplayers}
-					</Columns.Column>
-					<Columns.Column size="one-third">
-						<Icon><i className="fas fa-clock"></i></Icon>{
-							(neigh.neighbor.minplaytime && neigh.neighbor.maxplaytime && neigh.neighbor.minplaytime !== neigh.neighbor.maxplaytime)
-							? `${neigh.neighbor.minplaytime}-${neigh.neighbor.maxplaytime}` : neigh.neighbor.playingtime}
-					</Columns.Column>
-					<Columns.Column size="one-third">
-						<Icon><i className="fas fa-star"></i></Icon>{neigh.neighbor.bayesaverage.toFixed(2)}
-					</Columns.Column>
-				</Columns>
-			</Box>
+			<Card>
+				<Card.Content>
+					{this.props.show_image
+						? (<p className="has-text-centered"><img src={neigh.neighbor.thumbnail} /></p>)
+						: ""}
+					<h4><Link to={`/game/${neigh.neighbor.objectid}/`}>{neigh.neighbor.name}</Link></h4>
+					<Columns gapless>
+						<Columns.Column size="one-third">
+							<Icon><i className="fas fa-arrows-alt-h"></i></Icon>{neigh.distance.toFixed(3)}
+						</Columns.Column>
+						<Columns.Column size="one-third">
+							<Icon><i className="fas fa-globe"></i></Icon>{neigh.neighbor.usersrated}
+						</Columns.Column>
+						<Columns.Column size="one-third">
+							<a href={`https://boardgamegeek.com/boardgame/${neigh.neighbor.objectid}`}><Icon><i className="fas fa-external-link-alt"></i></Icon> BGG</a>
+						</Columns.Column>
+						
+						<Columns.Column size="one-third">
+							<Icon><i className="fas fa-users"></i></Icon>{Game.players(neigh.neighbor)}
+						</Columns.Column>
+						<Columns.Column size="one-third">
+							<Icon><i className="fas fa-clock"></i></Icon>{Game.playtime(neigh.neighbor)}
+						</Columns.Column>
+						<Columns.Column size="one-third">
+							<Icon><i className="fas fa-star"></i></Icon>{neigh.neighbor.bayesaverage.toFixed(2)}
+						</Columns.Column>
+					</Columns>
+				</Card.Content>
+			</Card>
 		);
 	}
 }
@@ -122,9 +136,22 @@ class Results extends React.Component {
 		} else {
 			return (
 				<Content>
-					<h4>Results for {game.name}:</h4>
+					<Columns gapless>
+						<Columns.Column size="two-thirds"><h4>Results: {game.name}</h4></Columns.Column>
+						<Columns.Column>
+						<Level breakpoint="mobile" className="is-marginless">
+								<Level.Item><a className="tooltip" data-tooltip={`Adjusted average of ${game.usersrated} ratings`}><Icon><i className="fas fa-star"></i></Icon>{game.bayesaverage.toFixed(2)}</a></Level.Item>
+								<Level.Item><Icon><i className="fas fa-users"></i></Icon>{Game.players(game)}</Level.Item>
+								<Level.Item><Icon><i className="fas fa-clock"></i></Icon>{Game.playtime(game)}</Level.Item>
+								<Level.Item><a href={`https://boardgamegeek.com/boardgame/${game.objectid}`}><Icon><i className="fas fa-external-link-alt"></i></Icon></a></Level.Item>
+						</Level>
+						</Columns.Column>
+					</Columns>
+					
 					<Columns multiline>
-					{game.gameneighbor_set.map((game_neighbor) => (
+					{game.gameneighbor_set.slice(0,3).map((game_neighbor) => (
+						<Columns.Column size="one-third" key={game_neighbor.neighbor.objectid}><Game show_image game_neighbor={game_neighbor} /></Columns.Column>
+					))}{game.gameneighbor_set.slice(3).map((game_neighbor) => (
 						<Columns.Column size="one-third" key={game_neighbor.neighbor.objectid}><Game game_neighbor={game_neighbor} /></Columns.Column>
 					))}
 					</Columns>
