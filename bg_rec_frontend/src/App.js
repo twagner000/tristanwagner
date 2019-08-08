3d12c1a8671cbbfc46562ea15f71cd206bf30f0d
twagner000 <twagner000@users.noreply.github.com>
2019-08-07 22:01:03 -0700

    use router

--- bg_rec_frontend/src/App.js
+++ bg_rec_frontend/src/App.js
@@ -2,13 +2,24 @@ import React from 'react';
 import './App.css';
 
 import 'react-bulma-components/dist/react-bulma-components.min.css';
-import { Section, Container, Heading, Button, Form, Icon} from 'react-bulma-components';
-import BGRecommenderService from "./BGRecommenderService";
+import { Section, Container, Heading, Form, Icon} from 'react-bulma-components';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import axios from 'axios';
 
-export class RecommendationSearch extends React.Component {
+const BASE_URL = '/bg_rec';
+
+export class Info extends React.Component {
+	render() {
+		return (<p className="content">Choose a game to get recommendations of similar games.</p>);
+	}
+}
+
+export class Search extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			game_list: [],
+			loaded: false,
 			game: "",
 			game_id: null
 		};
@@ -16,10 +27,15 @@ export class RecommendationSearch extends React.Component {
 		this.handleChange = this.handleChange.bind(this);
 	}
 	
+	componentDidMount() {
+        axios.get(`${BASE_URL}/api/game/`)
+			.then(response => this.setState({game_list: response.data, loaded: true}));
+	}
+	
 	handleChange(event) {
 		const search_lower_case = event.target.value.toLowerCase();
 		let game_id = null;
-		for (let g of this.props.game_list) {
+		for (let g of this.state.game_list) {
 			if (g.name.toLowerCase() === search_lower_case) {
 				game_id = g.objectid;
 				break;
@@ -36,23 +52,29 @@ export class RecommendationSearch extends React.Component {
 						<Form.Input placeholder="Select a game..." list="game_list" value={this.state.game} onChange={this.handleChange} />
 						<Icon align="left"><i className={this.state.game_id == null ? "fas fa-exclamation-triangle" : "fas fa-check"}></i></Icon>
 						<datalist id="game_list">
-							{this.props.game_list.map((game) => (<option key={game.objectid} value={game.name}/>))}
+							{this.state.game_list.map((game) => (<option key={game.objectid} value={game.name}/>))}
 						</datalist>
 					</Form.Control>
 					<Form.Control>
-						<Button onClick={() => this.props.onSearch(this.state.game_id)} color="primary" disabled={this.state.game_id == null ? true : false}><Icon><i className="fas fa-search"></i></Icon></Button>
+						<Link to={`${this.state.game_id}/`} className="button is-primary" disabled={this.state.game_id == null ? true : false}><Icon><i className="fas fa-search"></i></Icon></Link>
 					</Form.Control>
 				</Form.Field>
 				<Form.Field><Form.Help color="danger">{this.state.game_id == null ? "Please enter a valid game name." : ""}</Form.Help></Form.Field>
-				<p className="content">Choose a game to get recommendations of similar games.</p>
 			</React.Fragment>
 		);
 	}
 }
 
-export class RecommendationList extends React.Component {
+export class Results extends React.Component {
+	state = {game: null, loaded: false};
+	
+	componentDidMount() {
+        axios.get(`${BASE_URL}/api/game/${this.props.match.params.id}/`)
+			.then(response => this.setState({game: response.data, loaded: true}));
+	}
+	
 	render() {
-		let game = this.props.game;
+		let game = this.state.game;
 		if (!game) {
 			return "";
 		} else {
@@ -66,49 +88,24 @@ export class RecommendationList extends React.Component {
 					</ul>
 				</div>
 			);
+			
 		}
 	}
 }
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			service: new BGRecommenderService(),
-			game_list: [],
-			loaded: false,
-			game: null
-		};
-		
-		this.handleSearch = this.handleSearch.bind(this);
-	}
-	
-	componentDidMount() {
-		this.state.service.getGameList()
-			.then(data => this.setState({ game_list: data, loaded: true }));
-	}
-	
-	handleSearch(game_id) {
-		console.log(game_id);
-		this.state.service.getGameDetail(game_id)
-			.then(data => this.setState({ game: data }));
-	}
-	
 	render() {
 		return (
-			<Section>
-				<Container>
-					<Heading>Board Game Recommender</Heading>
-					{ this.state.loaded ? (
-						<React.Fragment>
-							<RecommendationSearch game_list={this.state.game_list} onSearch={this.handleSearch} />
-							<RecommendationList game={this.state.game} />
-						</React.Fragment>
-					) : (
-						<p>Loading game list...</p>
-					)}
-				</Container>
-			</Section>
+			<Router basename={BASE_URL}>
+				<Section>
+					<Container>
+						<Heading>Board Game Recommender</Heading>
+						<Search />
+						<Route exact path="/" component={Info} />
+						<Route path="/:id" component={Results} />
+					</Container>
+				</Section>
+			</Router>
 		);
 	}
 }
