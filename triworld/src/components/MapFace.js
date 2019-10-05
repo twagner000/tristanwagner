c84a54bbef26a862095ac29d94d82027a41a674f
twagner000 <twagner000@users.noreply.github.com>
2019-10-04 19:38:03 -0700

    improve scaling

--- triworld/src/components/MapFace.js
+++ triworld/src/components/MapFace.js
@@ -7,11 +7,15 @@ import {games} from "../actions";
 
 class MapFace extends React.Component {
 	render() {
-		let n = 6;
+		const box_width = 400;
+		const margin = 10;
+		const n = 6;
+		const b = (box_width-2*margin)/(8/3*n)*2; //scale triangle size
+		const h = b*Math.sqrt(3)/2;
+		
 		let row_n = [9,11,13,15,15,13,11,9];
-		let b = 30;
-		let h = b*Math.sqrt(3)/2;
 		let rows = new Array(row_n.length).fill(undefined).map((_,i) => new Array(row_n[i]).fill(undefined).map((_,j) => {return {sea: Math.random()<1/3, down:(i<2/3*n)===(j%2!==0)};}));
+		
 		return (
 			<Content>
 				<Link to="/" className="is-pulled-right delete"></Link>
@@ -19,7 +23,7 @@ class MapFace extends React.Component {
 				<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="400" height="400">
 					<rect width="100%" height="100%" style={{fill: 'none', strokeWidth: 1, stroke: '#000'}} />
 					{rows.map((r,ri) => (
-					<g key={ri} transform={"translate("+(Math.abs(2/3*n-.5-ri)-.5)*b/2+" "+ri*h+")"}>
+					<g key={ri} transform={"translate("+(margin+(Math.abs(2/3*n-.5-ri)-.5)*b/2)+" "+(margin+ri*h)+")"}>
 							{r.map((c,ci) => (
 								<g key={ci} transform={"translate("+b*ci/2+" 0)"}>
 									<path key={ci} d={"M 0 "+(c.down ? 0 : h)+" h "+b+" l "+(-b/2)+" "+(c.down ? h : -h)+" z"} className={"tri"+(c.sea ? " tri-sea" : " tri-land")} />
